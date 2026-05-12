e5ecfb332492a7cbd97299e68e59e1aca2229702
YuusyaIsami <macaronkunkun@gmail.com>
2026-05-12 21:58:32 +0900

    M3までの実装を完了

--- Assets/Art/Shader/EnvironmentStylizedLit/EnvironmentStylizedLit.shader
+++ Assets/Art/Shader/EnvironmentStylizedLit/EnvironmentStylizedLit.shader
@@ -3,7 +3,25 @@ Shader "BC/EnvironmentStylizedLit"
     Properties
     {
         [Enum(UnityEngine.Rendering.CullMode)] _Cull ("Cull", Float) = 2
+        [Toggle] _AlphaClip ("Alpha Clip", Float) = 0
+        _Cutoff ("Alpha Cutoff", Range(0, 1)) = 0.5
+
+        [MainTexture] _BaseMap ("Base Map", 2D) = "white" {}
         _BaseColor ("Base Color", Color) = (1, 1, 1, 1)
+
+        [Normal] _NormalMap ("Normal Map", 2D) = "bump" {}
+        _NormalScale ("Normal Scale", Range(0, 2)) = 1
+
+        _OcclusionMap ("Occlusion Map", 2D) = "white" {}
+        _OcclusionStrength ("Occlusion Strength", Range(0, 1)) = 1
+
+        _EmissionMap ("Emission Map", 2D) = "black" {}
+        [HDR] _EmissionColor ("Emission Color", Color) = (0, 0, 0, 1)
+        _EmissionStrength ("Emission Strength", Range(0, 10)) = 0
+
+        _Metallic ("Metallic", Range(0, 1)) = 0
+        _Smoothness ("Smoothness", Range(0, 1)) = 0.35
+        _SpecularColor ("Specular Color", Color) = (0.2, 0.2, 0.2, 1)
     }
 
     SubShader
@@ -24,12 +42,12 @@ Shader "BC/EnvironmentStylizedLit"
             Cull [_Cull]
             ZWrite On
             ZTest LEqual
-            ColorMask 0
 
             HLSLPROGRAM
 
             #pragma vertex ESL_Vertex
             #pragma fragment ESL_Fragment
+            #pragma multi_compile_fog
 
             #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
             #include "HLSL/Passes/EnvironmentStylizedLit_ForwardLitPass.hlsl"
--- Assets/Art/Shader/EnvironmentStylizedLit/HLSL/EnvironmentStylizedLit_Surface.hlsl
+++ Assets/Art/Shader/EnvironmentStylizedLit/HLSL/EnvironmentStylizedLit_Surface.hlsl
@@ -1,4 +1,78 @@
 #ifndef BC_ENVIRONMENT_STYLIZED_LIT_SURFACE_INCLUDED
 #define BC_ENVIRONMENT_STYLIZED_LIT_SURFACE_INCLUDED
 
+#include "EnvironmentStylizedLit_Input.hlsl"
+
+struct ESL_SurfaceData
+{
+	float3 albedo;
+	float alpha;
+
+	float3 normalTS;
+	float metallic;
+	float smoothness;
+	float occlusion;
+
+	float3 emission;
+
+	float cavity;
+	float colorVariationMask;
+	float bandOffsetMask;
+	float specialMask;
+};
+
+float2 ESL_TransformBaseUV(float2 uv)
+{
+	return uv * _BaseMap_ST.xy + _BaseMap_ST.zw;
+}
+
+void ESL_InitializeSurfaceData(out ESL_SurfaceData surfaceData)
+{
+	surfaceData.albedo = 1.0;
+	surfaceData.alpha = 1.0;
+	surfaceData.normalTS = float3(0.0, 0.0, 1.0);
+	surfaceData.metallic = 0.0;
+	surfaceData.smoothness = 0.35;
+	surfaceData.occlusion = 1.0;
+	surfaceData.emission = 0.0;
+	surfaceData.cavity = 0.0;
+	surfaceData.colorVariationMask = 0.0;
+	surfaceData.bandOffsetMask = 0.0;
+	surfaceData.specialMask = 0.0;
+}
+
+ESL_SurfaceData ESL_BuildSurfaceData(float2 uv)
+{
+	ESL_SurfaceData surfaceData;
+	ESL_InitializeSurfaceData(surfaceData);
+
+	float2 baseUV = ESL_TransformBaseUV(uv);
+	half4 baseSample = SAMPLE_TEXTURE2D(_BaseMap, sampler_BaseMap, baseUV);
+	half4 baseColor = baseSample * half4(_BaseColor);
+
+	surfaceData.albedo = baseColor.rgb;
+	surfaceData.alpha = baseColor.a;
+	surfaceData.normalTS = UnpackNormalScale(
+		SAMPLE_TEXTURE2D(_NormalMap, sampler_NormalMap, baseUV),
+		_NormalScale);
+
+	half occlusionSample = SAMPLE_TEXTURE2D(_OcclusionMap, sampler_OcclusionMap, baseUV).g;
+	surfaceData.occlusion = lerp(1.0, occlusionSample, saturate(_OcclusionStrength));
+
+	half3 emissionSample = SAMPLE_TEXTURE2D(_EmissionMap, sampler_EmissionMap, baseUV).rgb;
+	surfaceData.emission = emissionSample * _EmissionColor.rgb * _EmissionStrength;
+	surfaceData.metallic = saturate(_Metallic);
+	surfaceData.smoothness = saturate(_Smoothness);
+
+	return surfaceData;
+}
+
+void ESL_ApplyAlphaClip(float alpha)
+{
+	if (_AlphaClip > 0.5)
+	{
+		clip(alpha - _Cutoff);
+	}
+}
+
 #endif
